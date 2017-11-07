0b7bfb1298b51159faa21eb7e6d956c46f958693
semka.shumilov <semen.shumilov@mail.ru>
2017-11-07 14:16:42 +0300

    якорные ссылки в меню в хэдере

--- src/js/custom.js
+++ src/js/custom.js
@@ -189,6 +189,15 @@ $(".anchor").click(function() {
   return false;
 });
 
+$(".anchor-link").click(function() {
+  var elementClick = $(this).attr("href")
+  var destination = $(elementClick).offset().top;
+  jQuery("html:not(:animated),body:not(:animated)").animate({
+    scrollTop: destination
+  }, 800);
+  return false;
+});
+
 //slick
 $('.slider').slick({
   
