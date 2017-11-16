90c251236725f0554029bb22530dde49ca4475cb
semka.shumilov <semen.shumilov@mail.ru>
2017-11-16 13:31:31 +0300

    вынес скрипт для карты в файл scripts.html, заменил плагин jquery.mask на более новый

--- src/js/plugins.js
+++ src/js/plugins.js
@@ -1,4 +1,5 @@
 @@include('plugins/svg4everybody.js')
 @@include('plugins/jquery.validate.min.js')
-@@include('plugins/jquery.makedinput.js')
+
 @@include('plugins/slick.js')
+@@include('plugins/jquery.mask.js')
